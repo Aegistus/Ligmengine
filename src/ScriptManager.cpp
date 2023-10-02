198d9b55c9460de3fc3bdd4ad00a9a03e8d511f1
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2023-10-01 20:40:36 -0400

    Pre-AddComponent() Change Save

--- src/ScriptManager.cpp
+++ src/ScriptManager.cpp
@@ -4,6 +4,7 @@
 #include <ECS.h>
 #include <Components.h>
 #include "spdlog/spdlog.h"
+#include <Types.h>
 
 using namespace sol;
 
@@ -13,6 +14,35 @@ namespace Ligmengine
 	{
 		lua.open_libraries(lib::base, lib::math, lib::table);
 		lua.script("math.randomseed(0)");
+		// basic types
+		lua.new_usertype<vector3>("vector3",
+			constructors<vector3(), vector3(float), vector3(float, float, float)>(),
+			"x", &vector3::x,
+			"y", &vector3::y,
+			"z", &vector3::z,
+			// optional and fancy: operator overloading. see: https://github.com/ThePhD/sol2/issues/547
+			meta_function::addition, sol::overload([](const vector3& v1, const vector3& v2) -> vector3 { return v1 + v2; }),
+			meta_function::subtraction, sol::overload([](const vector3& v1, const vector3& v2) -> vector3 { return v1 - v2; }),
+			meta_function::multiplication, sol::overload(
+				[](const vector3& v1, const vector3& v2) -> vector3 { return v1 * v2; },
+				[](const vector3& v1, float f) -> vector3 { return v1 * f; },
+				[](float f, const vector3& v1) -> vector3 { return f * v1; }
+			)
+		);
+		lua.new_usertype<vector2>("vector2",
+			constructors<vector2(), vector2(float), vector2(float, float)>(),
+			"x", &vector2::x,
+			"y", &vector2::y,
+			// optional and fancy: operator overloading. see: https://github.com/ThePhD/sol2/issues/547
+			meta_function::addition, sol::overload([](const vector2& v1, const vector2& v2) -> vector2 { return v1 + v2; }),
+			meta_function::subtraction, sol::overload([](const vector2& v1, const vector2& v2) -> vector2 { return v1 - v2; }),
+			meta_function::multiplication, sol::overload(
+				[](const vector2& v1, const vector2& v2) -> vector2 { return v1 * v2; },
+				[](const vector2& v1, float f) -> vector2 { return v1 * f; },
+				[](float f, const vector2& v1) -> vector2 { return f * v1; }
+			)
+		);
+
 		// input manager functions
 		lua.set_function("GetKeyDown", [&](const InputCode inputCode) { return gEngine.input.GetKeyDown(inputCode); });
 		lua.set_function("GetKeyUp", [&](const InputCode inputCode) { return gEngine.input.GetKeyUp(inputCode);  });
@@ -21,14 +51,44 @@ namespace Ligmengine
 		lua.set_function("PlaySound", [&](const string& name) { return gEngine.soundManager.PlaySound(name); });
 		// ecs functions
 		lua.set_function("CreateEntity", [&]() { return gEngine.ecs.CreateEntity(); });
-		lua.set_function("DestroyEntity", [&]() { gEngine.ecs.DestroyEntity(); });
+		lua.set_function("DestroyEntity", [&](EntityID e) { gEngine.ecs.DestroyEntity(e); });
+		// COMPONENTS
+		lua.new_usertype<Transform>
+		(
+			"Transform",
+			constructors<Transform()>(),
+			"position", &Transform::position,
+			"rotation", &Transform::rotation,
+			"scale", &Transform::scale
+		);
+		lua.new_usertype<SpriteRenderer>
+		(
+			"SpriteRenderer",
+			constructors<SpriteRenderer()>(),
+			"sprite", &SpriteRenderer::sprite
+		);
+		lua.new_usertype<Script>
+			(
+				"Script",
+				constructors<Script()>(),
+				"name", &Script::name
+			);
 		// COMPONENT FUNCTIONS
 		// transform
-		lua.set_function("HasTransform", [&](EntityID e) {return gEngine.ecs.HasComponent<Transform>(e); });
-		lua.set_function("GetTransform", [&](EntityID e) {return gEngine.ecs.GetComponent<Transform>(e); });
-		lua.set_function("AddTransform", [&](EntityID e) { gEngine.ecs.AddComponent<Transform>(e); });
-		lua.set_function("RemoveTransform", [&](EntityID e) {gEngine.ecs.RemoveComponent<Transform>(e); });
+		lua.set_function("HasTransform", [&](EntityID e){return gEngine.ecs.HasComponent<Transform>(e); });
+		lua.set_function("GetTransform", [&](EntityID e)->Transform&{return gEngine.ecs.GetComponent<Transform>(e); });
+		lua.set_function("AddTransform", [&](EntityID e){ gEngine.ecs.AddComponent<Transform>(e); });
+		lua.set_function("RemoveTransform", [&](EntityID e){gEngine.ecs.RemoveComponent<Transform>(e); });
 		// sprite renderer
+		lua.set_function("HasSpriteRenderer", [&](EntityID e){return gEngine.ecs.HasComponent<SpriteRenderer>(e); });
+		lua.set_function("GetSpriteRenderer", [&](EntityID e)->SpriteRenderer& {return gEngine.ecs.GetComponent<SpriteRenderer>(e); });
+		lua.set_function("AddSpriteRenderer", [&](EntityID e){return gEngine.ecs.AddComponent<SpriteRenderer>(e); });
+		lua.set_function("RemoveSpriteRenderer", [&](EntityID e){return gEngine.ecs.RemoveComponent<SpriteRenderer>(e); });
+		// script
+		lua.set_function("HasScript", [&](EntityID e){return gEngine.ecs.HasComponent<Script>(e); });
+		lua.set_function("GetScript", [&](EntityID e)->Script& {return gEngine.ecs.GetComponent<Script>(e); });
+		lua.set_function("AddScript", [&](EntityID e){return gEngine.ecs.AddComponent<Script>(e); });
+		lua.set_function("RemoveScript", [&](EntityID e){return gEngine.ecs.RemoveComponent<Script>(e); });
 
 		// application functions
 		lua.set_function("Quit", [&]() { gEngine.quit = true; });
